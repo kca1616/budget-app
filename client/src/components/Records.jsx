80230519c727784a9bf278cc9ec022a50e52491b
Kiana Abachi <Kiana@Kianas-Macbook-Air.local>
2021-11-05 08:53:26 -0700

    display for records

---
+++ client/src/components/Records.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { getAllRecords } from "../services";
+
+const Records = () => {
+    const [records, setRecords] = useState([]);
+
+    useEffect(() => {
+        getAllRecords().then((fetchedRecords) => setRecords(fetchedRecords));
+    }, []);
+
+    return(
+        <section>
+            {records.map(record) => (
+                <div>
+                    <img src={record.albumArt} alt="album cover art"></img>
+                    <h3>{record.name}</h3>
+                    <h4>{record.yearPressed}</h4>
+                    <h5>{record.catalogNumber}</h5>
+                </div>
+            )}
+        </section>
+    )
+}
