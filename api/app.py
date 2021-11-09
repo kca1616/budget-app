d3450ff70de40e8a2f6a378123ddb605ea347e9a
Kiana Abachi <Kiana@Kianas-Macbook-Air.local>
2021-11-08 16:26:06 -0800

    remove more comments

--- api/app.py
+++ api/app.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 from flask_login import LoginManager
 import os
 
-# what do we import from locally?
 from db import DATABASE, initialize
 from record import Record
 from favorite import Favorite
@@ -11,13 +10,11 @@ from user import User
 from resources.records import record
 from resources.users import user
 
-# what options does our flask app need
 DEBUG = True
 PORT = 8000
 
 login_manager = LoginManager()
 
-# how do we create our flask app
 app = Flask(__name__)
 
 app.secret_key = os.environ.get('SECRET') or 'recordslongconfusingkey'
@@ -30,12 +27,9 @@ def load_user(userid):
     except:
         return None
 
-# set up before and after request decorators
 @app.before_request
 def before_request():
-    # make the global db be DATABASE
     g.db = DATABASE
-    # open the global db connection
     g.db.connect()
 
 @app.after_request
@@ -43,7 +37,6 @@ def after_request(response):
     g.db.close()
     return response
 
-# let's make an index route
 @app.route('/')
 def index():
     return 'Welcome to Record App!'
@@ -59,15 +52,11 @@ if 'DATABASE_URL' in os.environ:
     app.config['SESSION_COOKIE_SECURE'] = True
     app.config['SESSION_COOKIE_HTTPONLY'] = False
     app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-    # fetching the client url from an environment variable
     origins.append(os.environ.get('CLIENT_URL'))
 
 CORS(app, origins=origins, supports_credentials=True)
 
 
-# what's the main deal?
 if __name__ == '__main__':
-    # initialize tables
     initialize([Record, User, Favorite])
-    # run server
     app.run(debug=DEBUG, port=PORT)
