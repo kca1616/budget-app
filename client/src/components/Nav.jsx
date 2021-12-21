31cd219429b4b99a3a945d704fce879ff09b589d
Kiana Abachi <Kiana@Kianas-Macbook-Air.local>
2021-12-21 14:33:12 -0800

    fix navbar bug

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -12,16 +12,20 @@ const Nav = (props) => {
     }
 
     return (
-        <nav>
-            {/* <Link to="/" className="navItem">Home</Link> */}
-            {props.user == null ? <Link to="/login" className="navItem">Login</Link> : null}
-            {props.user == null ? <Link to="/register" className="navItem">Register</Link> : null}
-            {props.user !== null ? <Link to="/new" className="navItem">Add a new Record/Pressing</Link> : null}
-            {props.user !== null ? <Link to ="/wishlist" className="navItem">Wishlist</Link> : null}
-            {props.user !== null ?  <Link to="/records" className="navItem">Records</Link>: null}
-            {props.user !==null ? <button onClick={handleClick}>Log Out</button> : null}
-            
-        </nav>
+        <div>
+        {!props.user ? 
+            <nav>
+            <Link to="/login" className="navItem">Login</Link>
+            <Link to="/register" className="navItem">Register</Link>
+            </nav> : null}
+        {(props.user !== null && props.user !== undefined) ? 
+            <nav>
+                <Link to="/new" className="navItem">Add a new Record/Pressing</Link>
+                <Link to ="/wishlist" className="navItem">Wishlist</Link> 
+                <Link to="/records" className="navItem">Records</Link>
+                <button onClick={handleClick}>Log Out</button> 
+            </nav> : null}
+        </div>
     );
 };
 
